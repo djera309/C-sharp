501321afc7de6b946f3d0cb5dbd67c1d4cb81176
Elizaveta <djera309@yandex.ru>
2023-10-04 07:25:50 +0300

    added changes in numbers1

--- HelloConsole1/Numbers1/Program.cs
+++ HelloConsole1/Numbers1/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 4;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9;
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(result);
